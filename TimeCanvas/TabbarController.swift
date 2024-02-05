045c3d10dc700d369340b6042b373754519c8a49
Jimmy <myname90196@gmail.com>
2024-02-05 23:36:36 +0800

    -init- 1. Config tabbar controller 2. Use factory pattern to create viewcontroller and navigation controller

---
+++ TimeCanvas/TabbarController.swift
@@ -0,0 +1,116 @@
+//
+//  TabbarController.swift
+//  TimeCanvas
+//
+//  Created by JimmyChao on 2024/2/5.
+//
+
+import Foundation
+import UIKit
+
+protocol TabProtocol {
+    var defaultImage: UIImage { get }
+    var selectedImage: UIImage { get }
+    var bgColor: UIColor { get }
+    var title: String { get }
+}
+
+class TabbarController: UITabBarController {
+    
+    enum TabConfig: String, CaseIterable, TabProtocol {
+        case kanban = "kanban"
+        case chart = "analysis"
+        case profile = "profile"
+        
+        var title: String {
+            self.rawValue
+        }
+        
+        var defaultImage: UIImage {
+            switch self {
+            case .kanban:
+                UIImage(systemName: "list.bullet.clipboard")!
+            case .chart:
+                UIImage(systemName: "chart.bar")!
+            case .profile:
+                UIImage(systemName: "person.crop.circle")!
+            }
+        }
+        
+        var selectedImage: UIImage {
+            switch self {
+            case .kanban:
+                UIImage(systemName: "list.bullet.clipboard.fill")!
+            case .chart:
+                UIImage(systemName: "chart.bar.fill")!
+            case .profile:
+                UIImage(systemName: "person.crop.circle.fill")!
+            }
+        }
+        
+        var bgColor: UIColor {
+            switch self {
+            case .kanban:
+                UIColor.red
+            case .chart:
+                UIColor.blue
+            case .profile:
+                UIColor.green
+            }
+        }
+    }
+    
+    let tabConfigs: [TabConfig] = [.kanban, .chart, .profile]
+    let navFactory = NavControllerFactory()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = navFactory.createNavControllers(withEnumOfTabs: tabConfigs)
+        
+        // Setup Appearance
+        let tabBarApearance = UITabBarAppearance()
+        tabBarApearance.backgroundColor = .white
+        tabBar.scrollEdgeAppearance = tabBarApearance
+        tabBar.standardAppearance = tabBarApearance
+        
+    }
+}
+
+class NavControllerFactory {
+    
+    func createNavControllers(withEnumOfTabs tabConfigs: [TabProtocol]) -> [UINavigationController] {
+        let navControllers: [UINavigationController] = tabConfigs.enumerated().map { index, tabConfig in
+            let navigationController = createNavController(
+                color: tabConfig.bgColor,
+                tag: index,
+                defaultImage: tabConfig.defaultImage,
+                selectedImage: tabConfig.selectedImage)
+            
+            navigationController.viewControllers.first?.title = tabConfig.title
+            return navigationController
+        }
+        return navControllers
+    }
+    
+    private func createNavController(
+        color: UIColor,
+        tag: Int,
+        defaultImage: UIImage,
+        selectedImage: UIImage) -> UINavigationController{
+            
+            let newViewController = createViewController(with: color)
+            
+            let newNavController = UINavigationController(rootViewController: newViewController)
+            newNavController.tabBarItem.tag = tag
+            newNavController.tabBarItem.image = defaultImage
+            newNavController.tabBarItem.selectedImage = selectedImage
+            return newNavController
+        }
+    
+    private func createViewController(with color: UIColor) -> UIViewController {
+        let newViewController = UIViewController()
+        newViewController.view.backgroundColor = color
+        return newViewController
+    }
+}
